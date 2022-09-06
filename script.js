010ebc179ef1ec07e336bb498714decda0b4354d
Milton <miltonsoftwareengineer@gmail.com>
2022-09-06 22:32:50 +0300

    makes dynamic API calls

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ window.onload = () => {
     getTrendingNow()
     getTopRated()
   }
+
+  function fetchMovies(url, dom_element, path_type) {
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('something went wrong')
+        }
+      })
+      .then(data => {
+        showMovies(data, dom_element, path_type)
+      })
+      .catch(error_data => {
+        console.log(error_data)
+      })
+  }
