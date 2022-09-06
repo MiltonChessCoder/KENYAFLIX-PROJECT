dabf8a070c88ed760dd47fa0a35f2b709dcf1414
Milton <miltonsoftwareengineer@gmail.com>
2022-09-06 22:35:38 +0300

    adds event listeners and appends image element .

--- script.js
+++ script.js
@@ -20,3 +20,29 @@ window.onload = () => {
         console.log(error_data)
       })
   }
+
+  showMovies = (movies, dom_element, path_type) => {
+    
+    // Create a variable that grabs id or class
+    var moviesEl = document.querySelector(dom_element)
+  
+    // Loop through object
+    for (var movie of movies.results) {
+  
+      // Within loop create an img element
+      var imageElement = document.createElement('img')
+  
+      // Set attribute
+      imageElement.setAttribute('data-id', movie.id)
+  
+      // Set source
+      imageElement.src = `https://image.tmdb.org/t/p/original${movie[path_type]}`
+  
+      // Add event listener to handleMovieSelection() onClick
+      imageElement.addEventListener('click', e => {
+        handleMovieSelection(e)
+      })
+      // Append the imageElement to the dom_element selected
+      moviesEl.appendChild(imageElement)
+    }
+  }
